4207afc49a7d81362044ae45758544b328520991
Evelyne Colaço Garcia <evelynecolacogarcia1988@gmail.com>
2021-07-22 09:47:33 +0200

    exos

---
+++ 3.Jasmine/3.3.Recall/questions.js
@@ -0,0 +1,144 @@
+let selectElementsStartingWithA = (array) => {
+    function firstA(item) {
+        return item.toLowerCase().indexOf('a') === 0;
+      }
+
+      let startsWithA = array.filter(firstA);
+    return startsWithA;
+}
+
+let selectElementsStartingWithVowel = (array) => {
+    return array.filter(str => /^[aeiou]/i.test(str));;
+}
+
+let removeNullElements = (array) => {
+    var filtered = array.filter(function (el) {
+        return el != null;
+      });
+    return filtered;
+}
+
+let removeNullAndFalseElements = (array) => {
+              
+    return ;
+}
+
+let reverseWordsInArray = (array) => {
+    return 'Write your method here';
+}
+
+let everyPossiblePair = (array) => {
+    return 'Write your method here';
+}
+
+let allElementsExceptFirstThree = (array) => {
+    return 'Write your method here';
+}
+
+let addElementToBeginning = (array, element) => {
+    return 'Write your method here';
+}
+
+let sortByLastLetter = (array) => {
+    return 'Write your method here';
+}
+
+let getFirstHalf = (string) => {
+    return 'Write your method here';
+}
+
+let makeNegative = (number) => {
+    return 'Write your method here';
+}
+
+let numberOfPalindromes = (array) => {
+    return 'Write your method here';
+}
+
+let shortestWord = (array) => {
+    return 'Write your method here';
+}
+
+let longestWord = (array) => {
+    return 'Write your method here';
+}
+
+let sumNumbers = (array) => {
+    return 'Write your method here';
+}
+
+let repeatElements = (array) => {
+    return 'Write your method here';
+}
+
+let stringToNumber = (string) => {
+    return 'Write your method here';
+}
+
+let calculateAverage = (array) => {
+    return 'Write your method here';
+}
+
+let getElementsUntilGreaterThanFive = (array) => {
+    return 'Write your method here';
+}
+
+let convertArrayToObject = (array) => {
+    return 'Write your method here';
+}
+
+let getAllLetters = (array) => {
+    return 'Write your method here';
+}
+
+let swapKeysAndValues = (object) => {
+    return 'Write your method here';
+}
+
+let sumKeysAndValues = (object) => {
+    return 'Write your method here';
+}
+
+let removeCapitals = (string) => {
+    return 'Write your method here';
+}
+
+let roundUp = (number) => {
+    return 'Write your method here';
+}
+
+let formatDateNicely = (date) => {
+    return 'Write your method here';
+}
+
+let getDomainName = (string) => {
+    return 'Write your method here';
+}
+
+let titleize = (string) => {
+    return 'Write your method here';
+}
+
+let checkForSpecialCharacters = (string) => {
+    return 'Write your method here';
+}
+
+let squareRoot = (number) => {
+    return 'Write your method here';
+}
+
+let factorial = (number) => {
+    return 'Write your method here';
+}
+
+let findAnagrams = (string) => {
+    return 'Write your method here';
+}
+
+let convertToCelsius = (number) => {
+    return 'Write your method here';
+}
+
+let letterPosition = (array) => {
+    return 'Write your method here';
+}
